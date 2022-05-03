f5e242dfd69499b27d8b8920ce2fd70eeeb2b93e
Yash Mahale <yashmahale1702@gmail.com>
2022-05-03 13:55:02 +0530

    components

---
+++ pages/news/index.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
+function NewsPage() {
+	return (
+		<Fragment>
+			<h1 className="NewsPage Original">News Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/first">First news Item</Link>
+				</li>
+				<li>
+					<Link href="/news/second">Second news Item</Link>
+				</li>
+			</ul>
+		</Fragment>
+	);
+}
+
+export default NewsPage;
